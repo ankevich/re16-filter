eb28065a974419865f3ce3e1cd0946c240641af1
Annie <overheads165@gmail.com>
2023-01-21 18:15:29 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { useState } from "react";
+import styled from "styled-components";
 import projects from "./projects";
 import Toolbar from "./components/Toolbar";
 import ProjectList from "./components/ProjectList";
 
-const Portfolio = () => {
-  const filters = ["All", "Websites", "Flayers", "Business Cards"];
-  const { selected, changeSelection } = useState("All");
-
-  const { state, changeState } = useState([]);
-
-  return (
-    <>
-      <Toolbar filters={filters} />
-      <ProjectList projects={projects}/>
-    </>
-  );
-};
-
-
-
-
-
 function App() {
   return <Portfolio />;
 }
+
+const Portfolio = () => {
+  const filters = ["All", "Websites", "Flayers", "Business Cards"];
+  const { selected, changeSelection } = useState("All");
+  const { state, changeState } = useState([]);
+
+  return (
+    <Container>
+      <Toolbar filters={filters} />
+      <ProjectList projects={projects} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 980px;
+`;
 
 export default App;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,11 +1,27 @@
+import styled from "styled-components";
+
 const ProjectList = ({ projects }) => {
   return (
-    <>
+    <List>
       {projects.map((project) => {
         return <img src={project.img} alt={project.category} />;
       })}
-    </>
+    </List>
   );
 };
+
+const List = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  img {
+    width: 30%;
+    margin-bottom: 1rem;
+    object-fit: cover;
+  }
+
+`;
 
 export default ProjectList;
